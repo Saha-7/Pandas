9d37b2bf702a3d3f76f85f7d5c8d43902e37f104
saha-7op <saha7pritam@gmail.com>
2025-10-01 21:16:00 +0530

    info + describe

---
+++ 5. describe.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Hannah", "Ian", "Jack"],
+    "Age": [25, 30, 35, 40, 28, 33, 38, 29, 31, 27],
+    "Salary": [50000, 60000, 70000, 80000, 55000, 65000, 75000, 58000, 62000, 52000]
+}
+
+
+df = pd.DataFrame(data)
+
+# Generate descriptive statistics
+print(df.describe())
